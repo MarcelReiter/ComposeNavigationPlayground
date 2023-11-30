da56cc6f8f2d9059942ad46029bfd613fa5f2989
Marcel Reiter <m.reiter@anfe.ma>
2023-11-30 13:31:42 +0100

    Enhancement: add PlaygroundApplication

---
+++ app/src/main/java/com/anfema/composenavigationplayground/PlaygroundApplication.kt
@@ -0,0 +1,12 @@
+package com.anfema.composenavigationplayground
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class PlaygroundApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
